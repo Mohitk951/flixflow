0be00589435ae27aeea0158030421abbc51560ab
Mohit <98803372+Mohitk951@users.noreply.github.com>
2024-03-31 17:22:08 +0530

    Add files via upload

---
+++ flix-flow/flix-flow/backend/src/test/java/com/Team/tree/flixflow/FlixFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team.tree.flixflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
